94243b8737c65b36b17f19e2edea08296f5fc422
neqelovek <neqelovek@gmail.com>
2022-02-04 19:06:20 +0300

    To task 1,2,3,4 finish

--- app/src/main/java/ru/gb/lesson1kotlin/MainActivity.kt
+++ app/src/main/java/ru/gb/lesson1kotlin/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.gb.lesson1kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import ru.gb.lesson1kotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.text)
+        val enterText: EditText = findViewById(R.id.enter_text)
+        val button: Button = findViewById(R.id.button_click)
+
+        button.setOnClickListener { text.text = enterText.text }
     }
 }
